15e6ced916ee55e8b97e38ccfc81fef3f1399114
mariocyber17 <marioudea1988@gmail.com>
2025-11-09 09:10:53 -0500

    Edite los archivos desde miNuevaRama esto lo hago como practica 9/11/25

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 Hola mundo
 hola mundo desde miNuevaRama
+
+hola mundo desde miNuevaRama paracticando el 09/11/25
--- main.py
+++ main.py
@@ -5,3 +5,6 @@ print("hola mundo")
 
 
 print("HOLA MUNDO desde miNuevaRama")
+
+
+print(" esto lo estoy haciendo desde mi nueva rama x 2")
